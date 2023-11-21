29e84a78a9837f2c968e2f61a78a2b4f15bd47be
Ravi Teja <ravikanna183@gmail.com>
2023-11-21 14:24:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('h2');
-const navLinks = document.querySelectorAll('#sticky-nav a');
+const navLinks = document.querySelectorAll('#nav-overlay a');
 
 const observerOptions = {
     root: null,
@@ -10,12 +10,10 @@ const observerOptions = {
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            // Remove the "active" class from all nav links
             navLinks.forEach((link) => {
                 link.style.backgroundColor = "";
             });
 
-            // Find the corresponding nav link and add the "active" class
             const link = document.querySelector(`#sticky-nav a[href="#${entry.target.id}"]`);
             if (link) {
                 link.style.backgroundColor = "green";
@@ -34,50 +32,57 @@ sections.forEach((section) => {
     observer.observe(section);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Initially hide nav items
-    document.getElementById("primary-nav").style.display = "flex";
 
-    // Add click event listener to the "Show Content" button
-    document.getElementById("show-content-btn").addEventListener("click", function (event) {
-        toggleShowContent(event);
+document.addEventListener('DOMContentLoaded', function () {
+    const showContentBtn = document.getElementById('show-content-btn');
+    const navOverlay = document.getElementById('nav-overlay');
+
+    showContentBtn.addEventListener('click', function () {
+        navOverlay.classList.toggle('show');
     });
 
-    // Add scroll event listener to update progress bar and hide nav items
+    // Smooth scroll to section when a nav link is clicked
+    document.querySelectorAll('#primary-nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - document.getElementById('sticky-nav').offsetHeight,
+                behavior: 'smooth'
+            });
+
+            navOverlay.classList.remove('show');
+        });
+    });
+
     window.addEventListener("scroll", function () {
-        updateProgressBar();
-        hideNavOnScroll();
+        const scrollPercentage =
+            (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+        document.getElementById("progress-bar").style.width = scrollPercentage + "%";
+    });
+
+    // Scrollspy functionality to highlight the active section in the nav
+    window.addEventListener('scroll', function () {
+        const sections = document.querySelectorAll('.all-section');
+        let scrollPosition = window.scrollY;
+
+        sections.forEach((section, index) => {
+            if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+                document.querySelectorAll('#primary-nav a').forEach((navLink) => {
+                    navLink.classList.remove('active');
+                });
+
+                const currentNavLink = document.querySelectorAll('#primary-nav a')[index];
+                currentNavLink.classList.add('active');
+            }
+        });
     });
 });
 
-function toggleShowContent(event) {
-    const primaryNav = document.getElementById("primary-nav");
 
-    if (!primaryNav) return;
 
-    // Toggle visibility of nav items
-    primaryNav.style.display = primaryNav.style.display === "none" ? "flex" : "none";
-
-    window.scroll(0,0);
-}
-
-function updateProgressBar() {
-    const progressBar = document.getElementById('progress-bar');
-    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = (window.scrollY / scrollableHeight) * 100;
-    progressBar.style.width = `${scrolled}%`;
-}
-
-function hideNavOnScroll() {
-    const showContentBtn = document.getElementById("show-content-btn");
-    const primaryNav = document.getElementById("primary-nav");
-
-    if (!showContentBtn || !primaryNav) return;
-
-    // Check if show content button is not active, then hide nav items on scroll
-    if (!showContentBtn.classList.contains("active")) {
-        primaryNav.style.display = "none";
-    }
-}
 
 
